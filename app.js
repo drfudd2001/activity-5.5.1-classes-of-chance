14331ea07b6417eb944f0ef650caf9472d05de6b
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-04-11 18:31:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,64 @@
-class Casino {
-    // Write code here
-};
-
-// TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
-
-// BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
-console.log(myBonusCasino);
-myBonusCasino.playGame(5);
-myBonusCasino.playGame(15);
-myBonusCasino.playGame(25);
-myBonusCasino.playGame(35);
-*/
-
-// Extra BONUS TESTS
-/*
-const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
-console.log(myExtraBonusCasino);
-myExtraBonusCasino.rollDie(6);
-myExtraBonusCasino.rollDie(20);
-myExtraBonusCasino.rollDie(100);
-*/
+class Vehicle {
+    constructor(vehicleType, make, model, year)
+        vehicleType = this.vehicleType;
+        numberOfWheels {this.numberOfWheels = 0};
+        this.numberOfPassengers = 0;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.fuel = 0;
+        this.started = false;
+        this.speed = 0;
+        this.milleage = milleage;
+}
+  
+start() {
+    if (this.fuel > 0 {
+      return this.started = true;
+      console.log ("Engine started!");
+    } else {
+        return this.started = false;
+        console.log ("Engine did not start. Needs fuel!");
+    }
+  }
+  
+  accelerate(){
+    if (this.started) {
+      if(this.fuel > 0) {
+        console.log (this.speed += 1);
+        this.fuel = this.fuel - 1;
+    } else {
+        console.log ("Vehicle is empty, needs fuel.");
+        this.stop();
+      }
+    } else {
+        alert ("Engine not started.")
+    }
+  }
+  
+  decelerate(){
+    if (this.started) {
+      if (this.fuel > 0) {
+        if (this.speed > 0) {
+          console.log (this.speed -= 1);
+          this.fuel = this.fuel -1;
+        } else {
+            console.log ("Vehicle stopped. Out of fuel.");
+            this.stop();
+              }
+          } else {
+              alert("You need to start the engine first.");
+          }
+        }
+    }
+  }
+    stop() {
+        this.started = false;
+    }
+    drive() {
+        accelerate();
+    }
+    brake() {
+        decelerate();
+    }
+  
